727ff770325e8c2a8ddf44096bb83786d38a3110
王坤 <wkbjlyzx@163.com>
2018-06-20 18:01:30 +0800

    delete useless code

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -10,8 +10,6 @@ let _cap = 20 * Math.pow(10,18);
 let _initialSupply = 10000000000;
 module.exports = function(deployer, network,accounts) {
    deployer.deploy(UPToken,_initialSupply).then(function(){
-       /*uint256 _startTime, uint256 _endTime, uint256 _rate, uint256 _goal,
-           uint256 _cap, address _wallet, ERC20 _token*/
        let _wallet=accounts[accounts.length-3];
        let _token=UPToken.address;
        return deployer.deploy(UDAPCrowdsale,_startTime, _endTime,_rate,_goal,_cap,_wallet,UPToken.address);
