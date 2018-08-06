1e3e95f5ac8df7b96305398774f8a5e75a49f625
王坤 <wkbjlyzx@163.com>
2018-08-06 14:28:23 +0800

    add Airdrop contract

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -5,45 +5,33 @@ import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
 
 contract Airdrop is Ownable{
 
-    ERC20 public token;
-    uint256 public decimals;
-    uint8 public maxCount = 50;
-    address private wallet = "0xcabe9a163b96865308605bde13233fd1a0610931";
 
-    constructor(ERC20 _token) public {
-        require(_token != address(0));
-        token = _token;
-        decimals = _token.decimals();
+    mapping(address => bool) public airdrops;
+
+    ERC20 public tokenContract;
+
+    constructor(ERC20 _tokenContract) public {
+        require(_tokenContract != address(0));
+        tokenContract = _tokenContract;
     }
+
 
     /**
     * @dev perform a transfer of allocations
     * @param _recipients is a list of recipients
     * @param _tokens is a list of tokens
     */
-    function airdropTokens(address[] _recipients,uint256[] _tokens) public onlyOwner {
+    function airdropTokens(address[] _recipients,uint256[] _tokens,bool allowMultiple) public  {
         require(_recipients.length == _tokens.length);
-        require(_recipients.length <= maxCount);
 
-        for(uint256 i = 0; i< _recipients.length; i++)
-        {
-            if (_tokens[i] > 0) {
-                token.transfer(_recipients[i], _tokens[i] * decimals);
+        for(uint8 i = 0; i< _recipients.length; i++){
+            if (_tokens[i] > 0 && (allowMultiple || !airdrops[_recipients[i]]) ) {
+                tokenContract.transferFrom(msg.sender,_recipients[i], _tokens[i]);
+                airdrops[_recipients[i]] = true;
             }
         }
     }
 
-    function setMaxCount(uint8 _maxCount) public onlyOwner {
-       maxCount =  _maxCount;
-    }
-
-
-    function withdraw() public onlyOwner {
-        uint256 remaining = token.balanceOf(this);
-        if (remaining > 0) {
-            token.transfer(wallet, remaining);
-        }
-    }
 
 
 }
